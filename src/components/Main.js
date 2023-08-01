388e3321e79912333fa1313f05a4353b2985d33c
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-02 00:38:42 +0300

    opening-page-update

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,6 +27,7 @@ function Main() {
             <Router>
                 <NavbarSite />
                 <Routes>
+                    <Route path="/" element={<Home />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
