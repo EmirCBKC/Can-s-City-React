a0600c6880c3d0e091237f9f6dbc4ffac09978fe
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-02 17:50:54 +0300

    detail-update

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,9 +4,9 @@ import GamesData from './GamesData';
 
 function Detail() {
     const { id } = useParams();
-
-    // GamesData dizisinde tıklanan id'ye sahip ürünü arayıp döndürelim
     const element = GamesData.find((item) => item.id === parseInt(id));
+    const elementDeluxe = GamesData.find((item) => item.edition.edition_name === "DELUXE" && item.edition.game_name === element.edition.game_name);
+    const elementUltimate = GamesData.find((item) => item.edition.edition_name === "COLLECTOR" && item.edition.game_name === element.edition.game_name);
 
     return (
         <>
@@ -97,27 +97,75 @@ function Detail() {
                         </div>
                         <div className="deluxe-col mt-3 mb-3 col-xxl-4 col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
                             <div className="deluxe-detail">
-
+                                <div className="deluxe-game d-flex flex-column align-items-center justify-content-end" style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    border: "4px solid #00ffff",
+                                    backgroundImage: `url(${elementDeluxe.edition.img})`,
+                                    backgroundPosition: "center",
+                                    backgroundSize: "cover",
+                                    borderRadius: "30px",
+                                    boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"
+                                }}>
+                                    <div className="d-game p-4 d-flex flex-column align-items-center">
+                                        <h1 className="edition">{elementDeluxe.edition.edition_name}</h1>
+                                        <ul className="mt-4">
+                                            <li>Id consequuntur accusantium aliquid voluptatibus nisi odit dolorum sed
+                                                cumque
+                                                minima porro tempore.</li>
+                                            <li>Id consequuntur accusantium aliquid voluptatibus nisi odit dolorum sed
+                                                cumque
+                                                minima porro tempore.</li>
+                                        </ul>
+                                        <h2 className="text-center price mt-2">{elementDeluxe.edition.price}$</h2>
+                                        <button id={elementDeluxe.id} className="add-basket mt-2" value={elementDeluxe.edition.game_name}>Add Basket</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div className="ultimate-col mt-3 mb-3 col-xxl-4 col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12">
                             <div className="ultimate-detail">
-
+                                <div className="ultimate-game d-flex flex-column align-items-center justify-content-end" style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    border: "4px solid #00ffff",
+                                    backgroundImage: `url(${elementUltimate.edition.img})`,
+                                    backgroundPosition: "center",
+                                    backgroundSize: "cover",
+                                    borderRadius: "30px",
+                                    boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"
+                                }}>
+                                    <div className="u-game p-4 d-flex flex-column align-items-center">
+                                        <h1 className="edition">{elementUltimate.edition.edition_name}</h1>
+                                        <ul className="mt-4">
+                                            <li>Id consequuntur accusantium aliquid voluptatibus nisi odit dolorum sed
+                                                cumque
+                                                minima porro tempore.</li>
+                                            <li>Id consequuntur accusantium aliquid voluptatibus nisi odit dolorum sed
+                                                cumque
+                                                minima porro tempore.</li>
+                                        </ul>
+                                        <h2 className="text-center price mt-2">{elementUltimate.edition.price}$</h2>
+                                        <button id={elementUltimate.id} className="add-basket mt-2" value={elementUltimate.edition.game_name}>Add Basket</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div className="detail-bottom-row mt-5 mb-5 row justify-content-center">
                         <div className="mt-3 mb-3 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div className="image-detail" style={{width: "100%",
-                        height: "40rem",
-                        backgroundColor: "rgba(46, 0, 86, 0.332)",
-                        borderRadius: "40px",
-                        border: "2px solid #00ffff",
-                        backgroundImage: `url(${element.image})`,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                        boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"}}>
+                            <div className="image-detail" style={{
+                                width: "100%",
+                                height: "40rem",
+                                backgroundColor: "rgba(46, 0, 86, 0.332)",
+                                borderRadius: "40px",
+                                border: "2px solid #00ffff",
+                                backgroundImage: `url(${element.image})`,
+                                backgroundPosition: "center",
+                                backgroundSize: "cover",
+                                boxShadow: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"
+                            }}>
                             </div>
                         </div>
                         <div className="mt-3 mb-3 col-xxl-6 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-xs-12">
